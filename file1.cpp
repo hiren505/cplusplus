251378d72afa8415e8dd8c61a34859a700746178
hiren_1 <hiren.unai@gmail.com>
2017-02-06 16:28:53 -0500

    take input and print it

--- file1.cpp
+++ file1.cpp
@@ -5,8 +5,11 @@ using namespace std;
 int main()
 {
 
-	cout<<"\nAll set for git";
-	
+	cout<<"\nAll set for git\n";
+	int a;
+	cout<<"\nEnter the value of a :";
+	cin>>a;
+	cout<<"\nThe value of a is : "<<a<<"\n";
 
 }
 
