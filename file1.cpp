da535a618ae918fb8fc463b2758634eb8e6b7329
hiren_1 <hiren.unai@gmail.com>
2017-02-17 18:09:19 -0500

    start of Class

--- file1.cpp
+++ file1.cpp
@@ -1,38 +1,52 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-void add()
-{
-	int a,b;
-	cout<<"\nEnter your first value  :";
-	cin>>a;
-	cout<<"\nEnter your second value :"
-	cin>>b;
-	cout<<"\nAddition of two numbers is : "<<a+b<<"\n";
 
+class school
+{
+
+	private:
+		int student_id;
+		char first_name[15];
+		char last_name[15];
+	
+	public:
+		// Default Constructor
+		school();
+
+		// constructor when student id is provided
+		school(int student_id);					
+			
+		void display();
+
+}; 
+
+school::school()
+{
+	cout<<"\nDefault constructor called\n";
 }
-void sub()
-{
-	int a,b;
-	cout<<"\nEnter your first value  :";
-	cin>>a;
-	cout<<"\nEnter your second value :"
-	cin>>b;
-	cout<<"\nSubtraction of two numbers is : "<<a-b<<"\n";
 
+school::school(int student_id_passed)
+{
+	student_id = student_id_passed;
+	cout<<"\nStudent ID: "<<student_id<<endl;
+	cout<<"\nEnter your First name : ";
+	cin.getline(first_name,15);
+	cout<<"\nEnter your last name  : ";
+	cin.getline(last_name,15);
 }
 
-
-void sub()
+void school::display()
 {
+	cout<<"\n\nStudent ID: "<<student_id;
+	cout<<"\nStudent First Name : "<<first_name;
+	cout<<"\nStudent Last Name  : "<<last_name<<endl;
 }
 
 int main()
 {
-
-	add();
-
+	int id = 123;
+	school s1(123);
+	s1.display();
 }
-
-
